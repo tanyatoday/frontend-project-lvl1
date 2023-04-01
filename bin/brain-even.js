490b7b75e9b4491639c9b25d92e16844efb121a4
Татьяна <tbazhenova4@gmail.com>
2023-04-01 16:03:59 +0700

    update evenGame

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import greeting from '../src/cli.js';
 import evenGame from '../src/game/even.js';
 
-greeting();
 evenGame();
--- src/game/even.js
+++ src/game/even.js
@@ -1,24 +1,21 @@
-import readlineSync from 'readline-sync';
-
-// генерация случайного числа
-const getRandomInt = (max) => Math.floor(Math.random() * max);
+import game from '../index.js';
+import getRandomNum from '../getRandomNum.js';
 
 // определяем чётность числа
 const isEven = (num) => num % 2 === 0;
 
-const evenGame = () => {
-  const description = 'Answer "yes" if number is even, otherwise answer "no".';
-  console.log(description);
+// задание
+const description = 'Answer "yes" if number is even, otherwise answer "no".';
 
-  const number = getRandomInt(100);
-  const question = `Question: ${number}`;
-  console.log(question);
-  const yourAnswer = readlineSync.question('Your answer: ');
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-  if (yourAnswer === correctAnswer) {
-    return console.log('Correct!');
-  }
-  return console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+// игра
+const evenGame = () => {
+  const taskEven = () => {
+    const number = getRandomNum(100);
+    const question = `${number}`;
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    return [question, correctAnswer];
+  };
+  game(description, taskEven);
 };
 
 export default evenGame;
