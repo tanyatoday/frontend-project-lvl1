9e78744c0c261023df86d9110e88b1b9c1197837
Татьяна <tbazhenova4@gmail.com>
2023-04-01 16:04:35 +0700

    add index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import readlineSync from 'readline-sync';
+
+const game = (description, task) => {
+  // приветствие
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  // задание
+  console.log(description);
+
+  // цикл вопросов
+  for (let i = 0; i < 3; i += 1) {
+    const question = task();
+
+    console.log(`Question: ${question[0]}`);
+
+    // ответ пользователя
+    const yourAnswer = readlineSync.question('Your answer: ');
+
+    // правильный ответ
+    const correctAnswer = question[1];
+
+    // сравнение ответов
+    if (yourAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default game;
