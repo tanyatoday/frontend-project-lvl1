2ad77a4f5f4319364434706223f94fe7058ab234
Татьяна <tbazhenova4@gmail.com>
2023-05-15 21:48:07 +0700

    fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,14 +4,14 @@ import getRandomNum from '../getRandomNum.js';
 // задание
 const description = 'What is the result of the expression?';
 
+// математические символы
+const operators = ['+', '-', '*'];
+const operator = operators[getRandomNum(2)];
+
 const calcGame = () => {
   const task = () => {
     const num1 = getRandomNum(50);
     const num2 = getRandomNum(50);
-
-    // математические символы
-    const operators = ['+', '-', '*'];
-    const operator = operators[getRandomNum(2)];
 
     // вопрос
     const question = `${num1} ${operator} ${num2}`;
