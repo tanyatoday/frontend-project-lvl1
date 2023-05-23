b78ec2926c7fb07b182c0c887af0ee4619dc5d54
Татьяна <tbazhenova4@gmail.com>
2023-05-23 19:05:19 +0700

    fix gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,20 +4,20 @@ import getRandomNum from '../getRandomNum.js';
 // задание
 const description = 'Find the greatest common divisor of given numbers.';
 
+const getGcd = (n1, n2) => {
+  if (n1 % n2 === 0) {
+    return n2;
+  }
+
+  return getGcd(n2, n1 % n2);
+};
+
 const gcdGame = () => {
   const task = () => {
     const num1 = getRandomNum(50);
     const num2 = getRandomNum(50);
 
     const question = `${num1} ${num2}`;
-
-    const getGcd = (n1, n2) => {
-      if (n1 % n2 === 0) {
-        return n2;
-      }
-
-      return getGcd(n2, n1 % n2);
-    };
 
     const result = getGcd(num1, num2).toString();
 
