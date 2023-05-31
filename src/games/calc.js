31c3143c588079ef2a584d453491fe22175db580
Татьяна <tbazhenova4@gmail.com>
2023-05-31 16:40:06 +0700

    fis calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,32 +6,33 @@ const description = 'What is the result of the expression?';
 
 // математические символы
 const operators = ['+', '-', '*'];
-const operator = operators[getRandomNum(2)];
+const randomOperator = operators[getRandomNum(2)];
 
 const calcGame = () => {
   const task = () => {
-    const randomNum1 = getRandomNum(50);
-    const randomNum2 = getRandomNum(50);
+    const num1 = getRandomNum(50);
+    const num2 = getRandomNum(50);
 
     // вопрос
-    const question = `${randomNum1} ${operator} ${randomNum2}`;
+    const question = `${num1} ${randomOperator} ${num2}`;
 
     // арифметические операции
     let result = 0;
-    switch (operator) {
+    switch (randomOperator) {
       case '+':
-        result = randomNum1 + randomNum2;
+        result = num1 + num2;
         break;
       case '-':
-        result = randomNum1 - randomNum2;
+        result = num1 - num2;
         break;
       case '*':
-        result = randomNum1 * randomNum2;
+        result = num1 * num2;
         break;
       default:
         result = null;
     }
     result = result.toString();
+
     return [question, result];
   };
   game(description, task);
