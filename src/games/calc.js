dc6b7cb32e98be2574dbb3a20b40ca6e70b0ff43
Татьяна <tbazhenova4@gmail.com>
2023-05-31 18:46:41 +0700

    fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -8,33 +8,37 @@ const description = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
 const randomOperator = operators[getRandomNum(2)];
 
+// арифметические операции
+const calculation = (operator, num1, num2) => {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+      result = null;
+  }
+
+  return result.toString();
+};
+
 const calcGame = () => {
   const task = () => {
-    const num1 = getRandomNum(50);
-    const num2 = getRandomNum(50);
-
     // вопрос
-    const question = `${num1} ${randomOperator} ${num2}`;
+    const randomNum1 = getRandomNum(30);
+    const randomNum2 = getRandomNum(30);
+    const question = `${randomNum1} ${randomOperator} ${randomNum2}`;
+    const correctAnswer = calculation(randomOperator, randomNum1, randomNum2);
 
-    // арифметические операции
-    let result = 0;
-    switch (randomOperator) {
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      case '*':
-        result = num1 * num2;
-        break;
-      default:
-        result = null;
-    }
-    result = result.toString();
-
-    return [question, result];
+    return [question, correctAnswer];
   };
+
   game(description, task);
 };
 
